3a29c9a35b409b7b19b837faa09c4e7e65b28d6c
jjkrlh308-cyber <jjkrlh.308@gmail.com>
2026-01-05 12:31:50 -0400

    Create Home component for Lanches Rogerinho

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Lanches Rogerinho</h1>
+      <p>Painel administrativo da lanchonete</p>
+    </main>
+  );
+}
